fe5bac154799edb79d53eb25e7159c6400c93561
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-13 22:45:22 -0500

    Brew formula update for eosc version v1.2.1

--- eosc.rb
+++ eosc.rb
@@ -1,9 +1,9 @@
 class Eosc < Formula
   desc "Cross-platform EOS command-line swiss-army-knife"
   homepage "https://eosc.app/"
-  url "https://github.com/eoscanada/eosc/releases/download/v1.2.0/eosc_1.2.0_macOS_x86_64.tar.gz"
-  version "1.2.0"
-  sha256 "87842f6df2c7388bcba6bbb3eabcf95b0c1436fb9e960c51e77e2a8d171397b7"
+  url "https://github.com/eoscanada/eosc/releases/download/v1.2.1/eosc_1.2.1_macOS_x86_64.tar.gz"
+  version "1.2.1"
+  sha256 "1758d5baf5395e9cf92ee9b30be01f5cde1da88de44d88dddb14431ebb8935ea"
 
   def install
     bin.install "eosc"
