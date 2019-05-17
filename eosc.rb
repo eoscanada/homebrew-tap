356f3ff41f6cc724e0bc32679ee032675a6a27e7
Matthieu Vachon <matt@eoscanada.com>
2019-05-17 15:54:32 -0400

    Updated to 1.3.0 release

--- eosc.rb
+++ eosc.rb
@@ -1,9 +1,9 @@
 class Eosc < Formula
   desc "Cross-platform EOS command-line swiss-army-knife"
   homepage "https://eosc.app/"
-  url "https://github.com/eoscanada/eosc/releases/download/v1.2.5/eosc_1.2.5_macOS_x86_64.tar.gz"
-  version "1.2.5"
-  sha256 "759a1948ccd79c8d9c4dc7cf94a4ceea100415546c80bc799dbfbef7088e2167"
+  url "https://github.com/eoscanada/eosc/releases/download/v1.3.0/eosc_1.3.0_macOS_x86_64.tar.gz"
+  version "1.3.0"
+  sha256 "87dd3a99da1a697694b7c32b791d68de8bc8b12f4e2ff2749b0df1ba2bcc6fd2"
 
   def install
     bin.install "eosc"
